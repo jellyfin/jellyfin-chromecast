fb6f97cddc7fe943b15403667a04fcb16bbd9a86
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-04-11 15:30:02 -0400

    update components

--- components/maincontroller.js
+++ components/maincontroller.js
@@ -1,4 +1,4 @@
-﻿define(['datetime', 'embyactions', 'browserdeviceprofile', '//www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js', '//www.gstatic.com/cast/sdk/libs/mediaplayer/1.0.0/media_player.js', 'cryptojs-sha1'], function (datetime, embyActions, browserDeviceProfileBuilder) {
+﻿define(['datetime', 'embyactions', 'browserdeviceprofile', '//www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js', '//www.gstatic.com/cast/sdk/libs/mediaplayer/1.0.0/media_player.js', 'cryptojs-sha1'], function (datetime, embyActions, browserdeviceprofile) {
 
     window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
     setInterval(updateTimeOfDay, 40000);
@@ -305,7 +305,7 @@
 
         if (reportEventType) {
 
-            var report = function () {
+            var report = function() {
                 embyActions.reportPlaybackProgress($scope, getReportingParams($scope));
             };
             embyActions.reportPlaybackProgress($scope, getReportingParams($scope), true, reportEventType);
@@ -665,9 +665,7 @@
 
     function getDeviceProfile(maxBitrate) {
 
-        var profile = browserDeviceProfileBuilder({
-            supportsCustomSeeking: true
-        });
+        var profile = browserdeviceprofile;
 
         profile.MaxStreamingBitrate = maxBitrate;
         profile.MaxStaticBitrate = maxBitrate;
