0a7b41cd08eb7bcf99f96bb30689c81e9080a2e0
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2020-04-06 00:11:53 +0200

    Convert castDevices to ES6 module

--- src/components/castDevices.js
+++ src/components/castDevices.js
@@ -1,41 +1,31 @@
-define([], function () {
-    "use strict";
-    
-    const castContext = cast.framework.CastReceiverContext.getInstance();
+const castContext = cast.framework.CastReceiverContext.getInstance();
 
-    //Device Ids
-    const GEN1n2 = 1;
-    const AUDIO = 2;
-    const GEN3 = 3;
-    const ULTRA = 4;
-    const NESTHUB = 5;
+// Device Ids
+export const deviceIds = {
+    GEN1n2: 1,
+    AUDIO: 2,
+    GEN3: 3,
+    ULTRA: 4,
+    NESTHUB: 5
+}
 
-    /**
-     * Get device id of the active Cast device.
-     * Tries to identify the active Cast device by testing different codec supports.
-     * @returns {number} Active Cast device Id.
-     */
-    function getActiveDeviceId() {
-        if (castContext.canDisplayType("video/mp4", "hev1.1.6.L153.B0") &&
+
+/**
+ * Get device id of the active Cast device.
+ * Tries to identify the active Cast device by testing support for different codecs.
+ * @returns {number} Active Cast device Id.
+ */
+export function getActiveDeviceId() {
+    if (castContext.canDisplayType("video/mp4", "hev1.1.6.L153.B0") &&
         castContext.canDisplayType("video/webm", "vp9")) {
-            return ULTRA;
-        } else if (castContext.canDisplayType("video/webm", "vp9")) {
-            return NESTHUB;
-        } else if (castContext.canDisplayType("video/mp4", "avc1.64002A")) {
-            return GEN3;
-        } else if (castContext.canDisplayType("video/mp4", "avc1.640029")) {
-            return GEN1n2;
-        } else {
-            return AUDIO;
-        }
+        return deviceIds.ULTRA;
+    } else if (castContext.canDisplayType("video/webm", "vp9")) {
+        return deviceIds.NESTHUB;
+    } else if (castContext.canDisplayType("video/mp4", "avc1.64002A")) {
+        return deviceIds.GEN3;
+    } else if (castContext.canDisplayType("video/mp4", "avc1.640029")) {
+        return deviceIds.GEN1n2;
+    } else {
+        return deviceIds.AUDIO;
     }
-
-    return {
-        GEN1n2,
-        AUDIO,
-        GEN3,
-        ULTRA,
-        NESTHUB,
-        getActiveDeviceId
-    };
-});
+}
