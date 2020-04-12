cb9b6d171163eea89f2b831f43f756860ce2307b
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2020-04-12 17:43:20 +0200

    Refactor out requirejs from app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
+import "./components/maincontroller";
+
 var DefaultMaxBitrate = 10000000;
 var MaxBitrate = null;
+
+// Just until we're able to deprecate this
+$scope = {};
 
 window.deviceInfo = {
     deviceId: "chromecast_" + new Date().getTime(),
@@ -24,74 +29,3 @@ function broadcastConnectionErrorMessage() {
         message: ""
     });
 }
-
-function initRequire(customPaths) {
-
-    console.log('Initializing requirejs');
-
-    var bowerPath = "../jellyfin-web/src/bower_components";
-    var embyWebComponentsBowerPath = bowerPath + '/emby-webcomponents';
-
-    var paths = {
-        browserdeviceprofile:  "./components/deviceprofilebuilder",
-        codecsupporthelper: "./components/codecsupporthelper",
-        castdevices: "./components/castDevices",
-        browser: embyWebComponentsBowerPath + "/browser",
-        jellyfinactions: './components/jellyfinactions',
-        maincontroller: './components/maincontroller',
-        apiclient: 'bower_components/emby-apiclient/apiclient',
-        fetchhelper: embyWebComponentsBowerPath + "/fetchhelper"
-    };
-
-    var urlArgs = "t=" + new Date().getTime();
-
-    var config = {
-
-        waitSeconds: 0,
-        urlArgs: urlArgs,
-
-        paths: paths,
-        map: {
-            '*': {
-                'css': embyWebComponentsBowerPath + '/requirecss',
-                'html': embyWebComponentsBowerPath + '/requirehtml'
-            }
-        }
-    };
-
-    requirejs.config(config);
-    
-    // mock this for now. not used in this app
-    define("globalize", [], function () {
-        return {
-            getCurrentLocale: function () {
-
-                if (navigator.language) {
-                    return navigator.language;
-                }
-                if (navigator.userLanguage) {
-                    return navigator.userLanguage;
-                }
-                if (navigator.languages && navigator.languages.length) {
-                    return navigator.languages[0];
-                }
-
-                return 'en-us';
-            }
-        };
-    });
-}
-
-function startApp() {
-
-    initRequire();
-
-    // Just until we're able to deprecate this
-    window.$scope = {};
-
-    require(['fetchhelper', 'maincontroller', 'helpers'], function (fetchhelper, maincontroller) {
-        window.fetchhelper = fetchhelper;
-    });
-}
-
-startApp();
