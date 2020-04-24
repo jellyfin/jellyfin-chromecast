be96086d8bc6d2b4fa5194bd45cf44603e33e636
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2020-04-24 19:22:01 +0200

    Add ESLint and config

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es6: true
+    },
+    extends: [
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: 'module'
+    },
+    rules: {
+        'block-spacing': ["error"],
+        'brace-style': ["error"],
+        'comma-dangle': ["error", "never"],
+        'comma-spacing': ["error"],
+        'eol-last': ["error"],
+        'indent': ["error", 4, { "SwitchCase": 1 }],
+        'keyword-spacing': ["error"],
+        'max-statements-per-line': ["error"],
+        'no-floating-decimal': ["error"],
+        'no-multi-spaces': ["error"],
+        'no-multiple-empty-lines': ["error", { "max": 1 }],
+        'no-trailing-spaces': ["error"],
+        'one-var': ["error", "never"],
+        'semi': ["error"],
+        'space-before-blocks': ["error"]
+    },
+    overrides: [{
+        files: ['./src/**/*.js'],
+        env: {
+            node: false,
+            browser: true,
+            es6: true
+        },
+        globals: {
+            cast: 'readonly',
+            PRODUCTION: 'readonly',
+            $scope: 'writable'
+        }
+    }]
+}
