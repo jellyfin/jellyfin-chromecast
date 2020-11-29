f7089fe1d939bcdf1dff67fa030ba3c303f03923
Simon Dobie <simon@oneblink.io>
2020-11-29 20:28:41 +1100

    remove unused eslint/prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint'],
     env: {
         node: true,
         es6: true
