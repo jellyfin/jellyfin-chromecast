2b2035ce3de714df89a4b453b0f3fa5abcdea6f9
Simon Dobie <simon@oneblink.io>
2020-11-29 19:44:47 +1100

    add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,36 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint'
-    ],
+    plugins: ['@typescript-eslint', 'prettier'],
     env: {
         node: true,
         es6: true
     },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
     ],
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module'
     },
     rules: {
-        'block-spacing': ["error"],
-        'brace-style': ["error"],
-        'comma-dangle': ["error", "never"],
-        'comma-spacing': ["error"],
-        'eol-last': ["error"],
-        'indent': ["error", 4, { "SwitchCase": 1 }],
-        'keyword-spacing': ["error"],
-        'max-statements-per-line': ["error"],
-        'no-floating-decimal': ["error"],
-        'no-multi-spaces': ["error"],
-        'no-multiple-empty-lines': ["error", { "max": 1 }],
-        'no-trailing-spaces': ["error"],
-        'one-var': ["error", "never"],
-        'semi': ["error"],
-        'space-before-blocks': ["error"]
+        'block-spacing': ['error'],
+        'brace-style': ['error'],
+        'comma-dangle': ['error', 'never'],
+        'comma-spacing': ['error'],
+        'eol-last': ['error'],
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'keyword-spacing': ['error'],
+        'max-statements-per-line': ['error'],
+        'no-floating-decimal': ['error'],
+        'no-multi-spaces': ['error'],
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'no-trailing-spaces': ['error'],
+        'one-var': ['error', 'never'],
+        semi: ['error'],
+        'space-before-blocks': ['error']
     },
     overrides: [
         {
@@ -48,9 +47,9 @@ module.exports = {
             },
             rules: {
                 // Disable these until we have converted the project to TS
-                '@typescript-eslint/explicit-module-boundary-types': "off",
-                '@typescript-eslint/no-explicit-any': "off"
+                '@typescript-eslint/explicit-module-boundary-types': 'off',
+                '@typescript-eslint/no-explicit-any': 'off'
             }
         }
     ]
-}
+};
---
+++ CONTRIBUTING.md
@@ -0,0 +1,15 @@
+# Contributing
+
+## Development
+
+### Environment
+
+The development environment is setup with editorconfig, Code style is enforced by prettier and eslint for Javascipt/Typescript linting
+
+-   [editorconfig](https://editorconfig.org/)
+-   [prettier](https://prettier.io/)
+-   [eslint](https://eslint.org/)
+
+## Pull Requests
+
+This project uses the standard Github Fork and PR flow
