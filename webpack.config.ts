c743025fe28b63ba5b0c6706d4087a4f46d8e6f1
Simon Dobie <simon@oneblink.io>
2020-12-17 05:18:10 +1100

    merge webpack config files into one, convert config to typescript

---
+++ webpack.config.ts
@@ -0,0 +1,87 @@
+/* eslint-env node */
+
+import * as path from 'path';
+import * as webpack from 'webpack';
+import { merge } from 'webpack-merge';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+import packagejson from './package.json';
+
+const common: webpack.Configuration = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './app.ts',
+    output: {
+        filename: '[name].[fullhash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            hash: false,
+            favicon: 'favicon.ico'
+        })
+    ],
+    module: {
+        rules: [
+            { test: /\.html$/, loader: 'html-loader' },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'file-loader'
+            },
+            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    }
+};
+
+const development: webpack.Configuration = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: process.env.RECEIVER_PORT
+            ? Number.parseInt(process.env.RECEIVER_PORT, 10)
+            : 9000,
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            PRODUCTION: JSON.stringify(false),
+            RECEIVERVERSION: JSON.stringify(packagejson.version)
+        })
+    ]
+};
+
+const production: webpack.Configuration = {
+    mode: 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            PRODUCTION: JSON.stringify(true),
+            RECEIVERVERSION: JSON.stringify(packagejson.version)
+        })
+    ]
+};
+
+module.exports = (env: string, argv: { [key: string]: string }) => {
+    let config;
+    if (argv.mode === 'production') {
+        config = merge(common, production);
+    } else {
+        config = merge(common, development);
+    }
+
+    return config;
+};
