743c1d9a310e6d038ddaec083abd4c2438256243
Fernando Fernández <ferferga.fer@gmail.com>
2020-12-31 13:26:35 +0100

    chore(lint): make linting stricter and auto lint on commit and save

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  syntax: 'css',
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'at-rule-no-unknown': null
+  }
+};
--- webpack.config.ts
+++ webpack.config.ts
@@ -7,102 +7,101 @@ import { merge } from 'webpack-merge';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
-import version from './package.json';
+import { version } from './package.json';
 
 const common: webpack.Configuration = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './app.ts',
-    output: {
-        filename: '[name].[fullhash].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: './'
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: [
-        // @ts-expect-error - Typings mismatch between versions
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
-            hash: false,
-            favicon: 'favicon.ico'
-        }),
-        new ImageMinimizerPlugin({
-            minimizerOptions: {
-                plugins: [
-                    [
-                        'svgo',
-                        {
-                            plugins: [
-                                {
-                                    removeComments: false
-                                }
-                            ]
-                        }
-                    ]
-                ]
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.ts',
+  output: {
+    filename: '[name].[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    // @ts-expect-error - Typings mismatch between versions
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      hash: false,
+      favicon: 'favicon.ico'
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeComments: false
+                }
+              ]
             }
-        })
-    ],
-    module: {
-        rules: [
-            { test: /\.html$/, loader: 'html-loader' },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: 'file-loader'
-            },
-            {
-                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                loader: 'file-loader'
-            },
-            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-            { test: /\.js$/, loader: 'source-map-loader' }
+          ]
         ]
-    }
+      }
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.html$/, loader: 'html-loader' },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader'
+      },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  }
 };
 
 const development: webpack.Configuration = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    // @ts-expect-error - Typings mismatch between versions
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: process.env.RECEIVER_PORT
-            ? Number.parseInt(process.env.RECEIVER_PORT, 10)
-            : 9000,
-        publicPath: '/'
-    },
-    plugins: [
-        new DefinePlugin({
-            PRODUCTION: JSON.stringify(false),
-            RECEIVERVERSION: JSON.stringify(version)
-        })
-    ]
+  mode: 'development',
+  devtool: 'inline-source-map',
+  // @ts-expect-error - Typings mismatch between versions
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: process.env.RECEIVER_PORT
+      ? Number.parseInt(process.env.RECEIVER_PORT, 10)
+      : 9000,
+    publicPath: '/'
+  },
+  plugins: [
+    new DefinePlugin({
+      PRODUCTION: JSON.stringify(false),
+      RECEIVERVERSION: JSON.stringify(version)
+    })
+  ]
 };
 
 const production: webpack.Configuration = {
-    mode: 'production',
-    plugins: [
-        new DefinePlugin({
-            PRODUCTION: JSON.stringify(true),
-            RECEIVERVERSION: JSON.stringify(version)
-        })
-    ]
+  mode: 'production',
+  plugins: [
+    new DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+      RECEIVERVERSION: JSON.stringify(version)
+    })
+  ]
 };
 
-module.exports = (
-    argv: { [key: string]: string }
-): webpack.Configuration => {
-    let config;
-    if (argv.mode === 'production') {
-        config = merge(common, production);
-    } else {
-        config = merge(common, development);
-    }
+module.exports = (argv: { [key: string]: string }): webpack.Configuration => {
+  let config;
 
-    return config;
+  if (argv.mode === 'production') {
+    config = merge(common, production);
+  } else {
+    config = merge(common, development);
+  }
+
+  return config;
 };
