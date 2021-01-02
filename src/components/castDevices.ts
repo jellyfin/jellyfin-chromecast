ae79a8f6593bef8e0a5c04305b6b83b40048df7e
hawken <hawken@thehawken.org>
2021-01-02 16:58:35 +0100

    Easing up DOM operations on CC Audio

--- src/components/castDevices.ts
+++ src/components/castDevices.ts
@@ -10,6 +10,9 @@ export enum deviceIds {
     CCGTV //Chromecast Google TV
 }
 
+// cached device id, avoid looking it up again and again
+let deviceId: number | null = null;
+
 /**
  * Get device id of the active Cast device.
  * Tries to identify the active Cast device by testing support for different codecs.
@@ -17,18 +20,21 @@ export enum deviceIds {
  * @returns Active Cast device Id.
  */
 export function getActiveDeviceId(): number {
+    if (deviceId !== null) return deviceId;
+
     if (
         castContext.canDisplayType('video/mp4', 'hev1.1.6.L153.B0') &&
         castContext.canDisplayType('video/webm', 'vp9')
     ) {
-        return deviceIds.ULTRA;
+        deviceId = deviceIds.ULTRA;
     } else if (castContext.canDisplayType('video/webm', 'vp9')) {
-        return deviceIds.NESTHUBANDMAX;
+        deviceId = deviceIds.NESTHUBANDMAX;
     } else if (castContext.canDisplayType('video/mp4', 'avc1.64002A')) {
-        return deviceIds.GEN3;
+        deviceId = deviceIds.GEN3;
     } else if (castContext.canDisplayType('video/mp4', 'avc1.640029')) {
-        return deviceIds.GEN1AND2;
+        deviceId = deviceIds.GEN1AND2;
     } else {
-        return deviceIds.AUDIO;
+        deviceId = deviceIds.AUDIO;
     }
+    return deviceId;
 }
