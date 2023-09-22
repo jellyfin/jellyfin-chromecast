710f4a8e9e9eef8bdba87eb38677e7f26c957e5c
Niels van Velzen <git@ndat.nl>
2023-09-22 23:29:42 +0200

    Run stylelint --fix

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     extends: ['stylelint-config-standard'],
     rules: {
-        'at-rule-no-unknown': null
+        'at-rule-no-unknown': null,
+        'selector-class-pattern': null,
+        'selector-id-pattern': null
     },
     syntax: 'css'
 };
