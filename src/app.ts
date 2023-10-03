3ab135d5aa13941f30a07da54ce6a38b767bd120
Niels van Velzen <git@ndat.nl>
2023-10-03 17:16:45 +0200

    Replace glyphicons with SVG file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { RepeatMode } from '@jellyfin/sdk/lib/generated-client/models/repeat-mode';
 
 import './components/maincontroller';
-import './css/glyphicons.css';
 import './css/jellyfin.css';
 
 window.mediaElement = document.getElementById('video-player');
