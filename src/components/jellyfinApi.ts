e53840ad93608aa610535c4a2f31935fea574588
Matthew Haughton <3flex@users.noreply.github.com>
2024-10-27 13:29:12 +1100

    Configure logging for Axios requests & responses

--- src/components/jellyfinApi.ts
+++ src/components/jellyfinApi.ts
@@ -1,5 +1,28 @@
+import { Api, Jellyfin } from '@jellyfin/sdk';
+import axios from 'axios';
 import { version as packageVersion } from '../../package.json';
 import { ajax } from './fetchhelper';
+
+axios.interceptors.request.use((request) => {
+    console.log(`requesting url: ${request.url}`);
+
+    return request;
+});
+
+axios.interceptors.response.use(
+    (response) => {
+        console.log(
+            `response status: ${response.status}, url: ${response.request.responseURL}`
+        );
+
+        return response;
+    },
+    // eslint-disable-next-line promise/prefer-await-to-callbacks
+    (error) => {
+        console.log(`request failed to url: ${error.request.url}`);
+        throw error;
+    }
+);
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export abstract class JellyfinApi {
