33a06d4e4e996d6c7cfcde5aea54b06850835727
Matthew Haughton <3flex@users.noreply.github.com>
2024-11-26 18:13:31 +1100

    Fix PlaybackState typing

--- src/components/playbackManager.ts
+++ src/components/playbackManager.ts
@@ -31,12 +31,12 @@ import type { ItemIndex, PlayRequest } from '~/types/global';
 export interface PlaybackState {
     startPositionTicks: number;
     mediaType: string | null | undefined;
-    itemId: string;
+    itemId: string | undefined;
 
     audioStreamIndex: number | null;
     subtitleStreamIndex: number | null;
     mediaSource: MediaSourceInfo | null;
-    mediaSourceId: string;
+    mediaSourceId: string | null;
     PlaybackMediaSource: MediaSourceInfo | null;
 
     playMethod: PlayMethod | undefined;
@@ -45,10 +45,10 @@ export interface PlaybackState {
     playNextItemBool: boolean;
 
     item: BaseItemDto | null;
-    liveStreamId: string;
+    liveStreamId: string | null;
     playSessionId: string;
 
-    runtimeTicks: number;
+    runtimeTicks: number | null;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
