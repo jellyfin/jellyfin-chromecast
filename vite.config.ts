36401db4398b9b523cd8cf34503b0fce78e93f5f
Niels van Velzen <git@ndat.nl>
2026-04-29 18:25:01 +0200

    Remove esbuild options

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
         target: 'es2015',
         assetsInlineLimit: 0
     },
-    esbuild: {
-        target: 'es2015'
-    },
     server: {
         port: 9000
     }
